3460dd8134a4adb823de79f3029fea37f15fe26e
guyue123 <huguyue_1981@163.com>
2017-10-02 12:01:38 +0800

    完善mysql查询服务

---
+++ com-base/com-base-service-mysql/com-base-service-mysql-provider/src/test/java/com/base/service/mysql/provider/AppTest.java
@@ -0,0 +1,14 @@
+package com.base.service.mysql.provider;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    public static void main( String[] args )  {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/service-provider.xml");  
+//        context.start();
+//        System.in.read();
+          com.alibaba.dubbo.container.Main.main(args);
+
+    }
+}
